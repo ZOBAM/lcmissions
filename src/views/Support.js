13a030ef68b3aceae3cb2827cb35387bf1bab5bc
ZOBAM <upc4you@gmail.com>
2021-04-25 07:07:09 +0100

    Working on the support page

--- src/views/Support.js
+++ src/views/Support.js
@@ -1,6 +1,6 @@
 export default function Support() {
   return (
-    <div className="p-6">
+    <div className="p-6" id="support">
       <h1 className="text-3xl font-medium bg-white px-4 pb-2 text-center">
         Be A Carrier of Good Tidings
       </h1>
@@ -14,7 +14,7 @@ export default function Support() {
         Bearing the above in mind, God made it clear to us the various ways
         anybody who feels led can be of help.
       </p>
-      <ul>
+      <ul className="list-disc">
         <li>
           <h2>Pray for The Missionaries</h2>
           <p>
@@ -36,8 +36,38 @@ export default function Support() {
           </p>
         </li>
         <li>Become a Missionary</li>
-        <li>Pay for The Missionary Work</li>
+        <p className="font-semibold">
+          (Rom 10:14)  How then shall they call on him in whom they have not
+          believed? and how shall they believe in him of whom they have not
+          heard? and how shall they hear without a preacher?
+        </p>
+        <li>
+          {" "}
+          <h2>Pay for The Missionary Work</h2>
+          <p>
+            The movement of Missionaries from one place to another can be helped
+            or hindered by the availability of money or the lack of it. The
+            transportation, the feeding, the medical and other humanitarian
+            service rendered at the mission field won't go well without
+            sufficient funds to run them.
+          </p>
+          <p>
+            Having the above in mind, it is clear that being a financial partner
+            of our missionary work is a very crucial and important support that
+            you can give and we humbly request that you consider it generously.
+          </p>
+          <h3>Account Details</h3>
+          <p>
+            Bank: First Bank <br /> Acc. No: 3096950187 <br /> Acc. Title:
+            Onawork Evangelical Team
+          </p>
+        </li>
       </ul>
+      <p>
+        Finally, let be clear by reminding you that you cannot lose your rewards
+        when you support the spreading of the gospel in which of the listed ways
+        above and we cannot outdo God in giving.
+      </p>
     </div>
   );
 }
